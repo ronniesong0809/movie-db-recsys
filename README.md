70188cbf41eb9a9852f1877a79ef7b4640e7f292
Ronnie Song <ronniesong0809@gmail.com>
2020-09-07 01:19:00 -0700

    retrieve movie metadata using TMDB api

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# movie-db-recommendation-system
+# movie-db-recsys
+
+This is a simple content-based recommendation system for The Movie Database website.
+
+## Installation
+```
+$ pip install -r requirements.txt
+```
+
+## License
+Apache License 2.0
---
+++ script.py
@@ -0,0 +1,21 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+url = 'https://api.themoviedb.org/3/movie/top_rated?api_key={}&language=en-US&page=1'.format(os.environ.get('TMDB_API_KEY'))
+headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+r = requests.get(url=url, headers=headers)
+
+response = r.json()
+
+res = list()
+for d in response['results']:
+    x = {}
+    x['id'] = d['id']
+    x['name'] = d['title']
+    x['description'] = d['overview']
+    res.append(x)
+
+print(res)
