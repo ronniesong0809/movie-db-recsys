b39b0627f39bc1b8cec3dadda9bc48dff0560f79
Ronnie Song <ronniesong0809@gmail.com>
2020-09-11 16:34:32 -0700

    increase the dataset size to 100

--- script.py
+++ script.py
@@ -6,20 +6,21 @@ import csv
 
 load_dotenv()
 
-url = 'https://api.themoviedb.org/3/movie/top_rated?api_key={}&language=en-US&page=1'.format(os.environ.get('TMDB_API_KEY'))
-headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
-r = requests.get(url=url, headers=headers)
-
-response = r.json()
-
 res = list()
-for d in response['results']:
-    x = {}
-    x['id'] = d['id']
-    x['name'] = d['title']
-    x['date'] = d['release_date']
-    x['description'] = d['overview']
-    res.append(x)
+for i in range(1, 7, 1):
+    url = 'https://api.themoviedb.org/3/movie/top_rated?api_key={}&language=en-US&page={}'.format(os.environ.get('TMDB_API_KEY'), i)
+    headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+    r = requests.get(url=url, headers=headers)
+
+    response = r.json()
+
+    for d in response['results']:
+        x = {}
+        x['id'] = d['id']
+        x['name'] = d['title']
+        x['date'] = d['release_date']
+        x['description'] = d['overview']
+        res.append(x)
 
 with open('data/data.json', 'w') as f:
     json.dump(res, f, indent=4)
