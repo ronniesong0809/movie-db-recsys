f84c69a883de2de76dea2857167cad166f477817
Ronnie Song <ronniesong0809@gmail.com>
2020-09-25 21:44:18 -0700

    return output instead of print out the output

--- demo.py
+++ demo.py
@@ -5,23 +5,34 @@ def load_obj(file):
     with open(file, 'rb') as f:
         return pickle.load(f)
 
-def getName(id):
-    return movies.loc[movies['id'] == id]['name'].tolist()
+# def getName(id):
+#     return movies.loc[movies['id'] == id]['name'].tolist()
 
-def getDesc(id):
-    return movies.loc[movies['id'] == id]['description'].tolist()
+# def getDesc(id):
+#     return movies.loc[movies['id'] == id]['description'].tolist()
+
+# def recommender(item_id, num):
+#     print('{} movies similar to {}'.format(num, getName(item_id)))
+#     print('---------------------------------------')
+#     recs = results[item_id][:num]
+#     for i, rec in enumerate(recs):
+#         print('Movie Id:    {}'.format(rec[1]))
+#         print('score:       {}'.format(rec[0]))
+#         print('Name:        {}'.format(getName(rec[1])))
+#         print('Description: {}\n'.format(getDesc(rec[1])))
 
 def recommender(item_id, num):
-    print('{} movies similar to {}'.format(num, getName(item_id)))
-    print('---------------------------------------')
+    py_list = list()
     recs = results[item_id][:num]
-    for i, rec in enumerate(recs):
-        print('Movie Id:    {}'.format(rec[1]))
-        print('score:       {}'.format(rec[0]))
-        print('Name:        {}'.format(getName(rec[1])))
-        print('Description: {}\n'.format(getDesc(rec[1])))
+    for rec in recs:
+        row = {}
+        row['id'] = rec[1]
+        row['score'] = rec[0]
+        py_list.append(row)
+    return py_list
 
-movies = pd.read_csv('data/data.csv', usecols = ['id', 'name', 'description'])
+# movies = pd.read_csv('data/data.csv', usecols = ['id', 'name', 'description'])
 
 results = load_obj('models/model.pkl')
-recommender(item_id = 155, num = 5)
+# recommender(item_id = 155, num = 5)
+print(recommender(item_id = 155, num = 5))
