38c788f1c41e2f37a551bcb56847de350451d100
Ronnie Song <ronniesong0809@gmail.com>
2020-09-25 20:43:03 -0700

    save model to file and load it later in order to make predictions usising pickle

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
+import pickle
 
 # load csv
 movies = pd.read_csv('data/data.csv', usecols = ['id', 'name', 'description'])
@@ -23,22 +24,6 @@ for idx, row in movies.iterrows():
     similar_items = [(cosine_similarities[idx][i], movies['id'][i]) for i in similar_indices]
     results[row['id']] = similar_items[1:]
 
-def getName(id):
-    return movies.loc[movies['id'] == id]['name'].tolist()
-
-def getDesc(id):
-    return movies.loc[movies['id'] == id]['description'].tolist()
-
-def recommend_prettier(item_id, num):
-    print('{} movies similar to {}'.format(num, getName(item_id)))
-    print('---------------------------------------')
-    recs = results[item_id][:num]
-    for i, rec in enumerate(recs):
-        print('Movie Id:    {}'.format(rec[1]))
-        print('score:       {}'.format(rec[0]))
-        print('Name:        {}'.format(getName(rec[1])))
-        print('Description: {}\n'.format(getDesc(rec[1])))
-
-movies = pd.read_csv('data/data.csv', usecols = ['id', 'name', 'description'])
-
-recommend_prettier(item_id = 155, num = 5)
+# save to pkl
+with open('models/model.pkl', 'wb') as f:
+    pickle.dump(results, f, pickle.HIGHEST_PROTOCOL)
