e56cbdc345a019c808dfad2d137c9eb96f9d7b1f
Ronnie Song <ronniesong0809@gmail.com>
2020-09-25 23:47:50 -0700

    add genre fields

--- script.py
+++ script.py
@@ -6,6 +6,13 @@ import csv
 
 load_dotenv()
 
+genre_data = json.load(open('genre.json'))
+
+def get_genre(id):
+    for i in genre_data:
+        if i['id'] == id:
+            return i['name']
+
 res = list()
 for i in range(1, 7, 1):
     url = 'https://api.themoviedb.org/3/movie/top_rated?api_key={}&language=en-US&page={}'.format(os.environ.get('TMDB_API_KEY'), i)
@@ -31,19 +38,24 @@ for i in range(1, 7, 1):
             x['description'] = d['overview'].replace("\r", " ")
         else:
             x['description'] = "null"
+        
+        genre = []
+        for i in d['genre_ids']:
+            g = get_genre(i)
+            genre.append(g)
+        x['genre'] = ', '.join(genre)
 
         res.append(x)
 
 with open('data/data.json', 'w') as f:
     json.dump(res, f, indent=4)
 
-
 data = []
 with open('data/data.json') as f:
     data = json.loads(f.read())
     f = csv.writer(open('data/data.csv', 'w', newline='', encoding='utf-8'))
 
-f.writerow(['id', 'name', 'date', 'description'])
+f.writerow(['id', 'name', 'date', 'genre', 'description'])
 
 for x in data:
-    f.writerow([x['id'], x['name'], x['date'], x['description']])
+    f.writerow([x['id'], x['name'], x['date'], x['genre'], x['description']])
